e04fefb8f4fdd0ca990ceb32341d8ab0ce3f7066
Peter Nichols <raspbian@itdiscovery.info>
2022-10-15 10:00:50 -0500

    Manually typed from RPi0

---
+++ ledtest.py
@@ -0,0 +1,19 @@
+from rpi_TM1638 import TMBoards
+from time import sleep
+DIO=19
+CLK=13
+STB=26
+
+TM=TMB oards(DIO,CLK,STB,0)
+TM.clearDisplay()
+LampDict = {0x01,0x03,0x05,0x07,0x09,0x0b,0x0d,0x0f}
+
+for x in LampDict:
+   TM.sendCommand(0x44,0)
+   TM.sendData(x,1,0)
+   TM.segments[4]="{:02}".format(x)
+   sleep(.5)
+   TM.sendData(x,2,0)
+   TM.segments[4]="{:02}".format(x+1)
+   TM.sendData(x,0,0)
+   //TM.sendData(x,3,0) I think turns on both
