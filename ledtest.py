d7a4f6bb9f02c5751807faeae3c87f34470a89d1
Peter Nichols <raspbian@itdiscovery.info>
2022-10-16 17:29:15 -0500

    Cut and paste from RPi4

--- ledtest.py
+++ ledtest.py
@@ -4,16 +4,23 @@ DIO=19
 CLK=13
 STB=26
 
-TM=TMB oards(DIO,CLK,STB,0)
+TM=TMBoards(DIO,CLK,STB,0)
 TM.clearDisplay()
 LampDict = {0x01,0x03,0x05,0x07,0x09,0x0b,0x0d,0x0f}
 
-for x in LampDict:
-   TM.sendCommand(0x44,0)
-   TM.sendData(x,1,0)
-   TM.segments[4]="{:02}".format(x)
-   sleep(.5)
-   TM.sendData(x,2,0)
-   TM.segments[4]="{:02}".format(x+1)
-   TM.sendData(x,0,0)
-   //TM.sendData(x,3,0) I think turns on both
+
+while True:
+  for y in range(7):
+     TM.turnOn(y)
+     for x in LampDict:
+       TM.sendCommand(0x44,0)
+       TM.sendData(x,1,0)
+       TM.segments[4]="{:02}".format(x)
+       sleep(.01)
+       TM.sendData(x,2,0)
+       TM.segments[4]="{:02}".format(x+1)
+       sleep(.01)
+       TM.sendData(x,3,0) #I think turns on both
+       sleep(.01)
+       TM.sendData(x,0,0)
+       sleep(.01)
